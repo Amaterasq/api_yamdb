4e6b0bb1e3aa942b0a5d48a37352944d5120e899
Bogov Alexey <bogov-alexey@yandex.ru>
2022-02-02 00:32:58 +0300

    Готово наполнение базы

--- api_yamdb/reviews/management/commands/dbfill.py
+++ api_yamdb/reviews/management/commands/dbfill.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
+from django.shortcuts import get_object_or_404
 
 import csv
 
-from reviews.models import Category, Comments
+from reviews.models import (User, Category, Titles, Review, Genre,
+                            GenreTitle, Comments)
 
 
 class Command(BaseCommand):
@@ -11,56 +13,125 @@ class Command(BaseCommand):
     """
     help = ('Helps to fill the database with test '
             'data from "/api_yamdb/static/data"')
-    titles_list = ('category', 'comments', 'genre_title',
-                   'genre', 'review', 'titles', 'users',)
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            'file_name', nargs='?', type=str, default=None
-        )
-
-    def parse_test_data(self, titles):
-        self.stdout.write(
-            f'Work with files: {titles}'
-        )
-        for title in titles:
-            with open(f'static/data/{title}.csv', encoding='utf-8') as csvfile:
-                reader = csv.reader(csvfile, delimiter=',')
-                # column_names = []
-                # column_count = 0
-                # row_count = 0
-                # for line_num, line in enumerate(reader):
-                #     if line_num == 0:
-                #         for i in range(len(line)):
-                #             column_names.append(line[i])
-                #         column_count = len(line)
-                for row_num, row in enumerate(reader):
-                    if row_num == 0:
-                        continue
-                    if title == 'category':
-                        Category.objects.get_or_create(
-                            id=row[0],
-                            name=row[1],
-                            slug=row[2],
-                        )
-                    elif title == 'comments':
-                        Comments.objects.get_or_create(
-                            id=row[0],
-                            review_id=row[1],
-                            text=row[2],
-                            author=row[3],
-                            pub_date=row[4]
-                        )
-                # self.stdout.write(
-                #     f'Столбцы: {column_names}, количество столбцов: {column_count}, количество записей: {row_count}'
-                # )
 
     def handle(self, *args, **options):
-        if options['file_name'] is None:
-            self.parse_test_data(self.titles_list)
-        else:
-            file_name = (options['file_name'],)
-            self.parse_test_data(file_name)
         self.stdout.write(
-            'Filling the database with test data was successful!'
+            'Starting to fill the database with test data...'
+        )
+        self.stdout.write(
+            '/api_yamdb/static/data/users.csv'
+        )
+        with open('static/data/users.csv', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row_num, row in enumerate(reader):
+                if row_num == 0:
+                    continue
+                else:
+                    User.objects.get_or_create(
+                        id=row[0],
+                        username=row[1],
+                        email=row[2],
+                        role=row[3],
+                        bio=row[4],
+                        first_name=row[5],
+                        last_name=row[6],
+                    )
+            csvfile.close()
+        self.stdout.write(
+            '/api_yamdb/static/data/category.csv'
+        )
+        with open('static/data/category.csv', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row_num, row in enumerate(reader):
+                if row_num == 0:
+                    continue
+                else:
+                    Category.objects.get_or_create(
+                        id=row[0],
+                        name=row[1],
+                        slug=row[2]
+                    )
+            csvfile.close()
+        self.stdout.write(
+            '/api_yamdb/static/data/titles.csv'
+        )
+        with open('static/data/titles.csv', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row_num, row in enumerate(reader):
+                if row_num == 0:
+                    continue
+                else:
+                    Titles.objects.get_or_create(
+                        id=row[0],
+                        name=row[1],
+                        year=row[2],
+                        category=get_object_or_404(Category, id=row[3])
+                    )
+            csvfile.close()
+        self.stdout.write(
+            '/api_yamdb/static/data/review.csv'
+        )
+        with open('static/data/review.csv', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row_num, row in enumerate(reader):
+                if row_num == 0:
+                    continue
+                else:
+                    Review.objects.get_or_create(
+                        id=row[0],
+                        title_id=get_object_or_404(Titles, id=row[1]),
+                        text=row[2],
+                        author=get_object_or_404(User, id=row[3]),
+                        score=row[4],
+                        pub_date=row[5]
+                    )
+            csvfile.close()
+        self.stdout.write(
+            '/api_yamdb/static/data/genre.csv'
+        )
+        with open('static/data/genre.csv', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row_num, row in enumerate(reader):
+                if row_num == 0:
+                    continue
+                else:
+                    Genre.objects.get_or_create(
+                        id=row[0],
+                        name=row[1],
+                        slug=row[2]
+                    )
+            csvfile.close()
+        self.stdout.write(
+            '/api_yamdb/static/data/genre_title.csv'
+        )
+        with open('static/data/genre_title.csv', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row_num, row in enumerate(reader):
+                if row_num == 0:
+                    continue
+                else:
+                    GenreTitle.objects.get_or_create(
+                        id=row[0],
+                        title_id=get_object_or_404(Titles, id=row[1]),
+                        genre_id=get_object_or_404(Genre, id=row[2])
+                    )
+            csvfile.close()
+        self.stdout.write(
+            '/api_yamdb/static/data/comments.csv'
+        )
+        with open('static/data/comments.csv', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row_num, row in enumerate(reader):
+                if row_num == 0:
+                    continue
+                else:
+                    Comments.objects.get_or_create(
+                        id=row[0],
+                        review_id=get_object_or_404(Review, id=row[1]),
+                        text=row[2],
+                        author=get_object_or_404(User, id=row[3]),
+                        pub_date=row[4]
+                    )
+        self.stdout.write(
+            'The database has been successfully filled with test data!'
         )
